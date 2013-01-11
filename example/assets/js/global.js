985e8c0878d60e991e0f1d08e6dd78e8ecb079a6
Aaron Gloege <agloege@nerdery.com>
2013-01-11 03:09:39 -0600

    Update example style.

--- example/assets/js/global.js
+++ example/assets/js/global.js
@@ -4,11 +4,17 @@
     $(document).ready(function() {
 
         var $touchInner = $('.touch-inner');
+        var parent = $touchInner.parent()[0];
 
         $('.touch-area').on('tap', function(e) {
             console.log('tap event', e);
             $touchInner.prepend("Tap {\n    x: " + e.pageX + ",\n    y: " + e.pageY + "\n};\n");
+            parent.scrollTop = 0;
         });
+
+        $('body').on('touchmove', '.touch-area', function(e) {
+            e.preventDefault();
+        })
 
     });
 
